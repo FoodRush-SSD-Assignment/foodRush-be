cfa06f4f2bbd582c8ebbda3b347f549b158c3d81
dinithii <dinithiwr@gmail.com>
2025-04-12 13:40:18 +0530

    server and db connection set

--- restaurant-service/index.js
+++ restaurant-service/index.js
@@ -1,1 +1,14 @@
-console.log('Hello, Node!');
+const express = require("express");
+const connectDB = require("./config/db");
+require("dotenv").config(); // Also here for safety
+
+const app = express();
+app.use(express.json());
+connectDB();
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on port ${PORT}`);
+});
+
