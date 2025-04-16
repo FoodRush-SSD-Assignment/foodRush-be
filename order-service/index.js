a06a9cd1f22289e5a8ff03d2ac46097d735d4a01
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2025-04-16 09:21:48 +0530

    set server and db connection

---
+++ order-service/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const connectDB = require("./config/db");
+require("dotenv").config();
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
