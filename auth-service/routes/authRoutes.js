b6a19a69167089f1b10cae10f026462e6246ffd0
Senith Udara <senithudara0000@gmail.com>
2025-04-23 23:16:59 +0530

    feat/resending verification mail apis created

--- auth-service/routes/authRoutes.js
+++ auth-service/routes/authRoutes.js
@@ -6,6 +6,7 @@ const {
   login,
   getAllUsers,
   getUserById,
+  resendVerificationCode,
 } = require("../controllers/authController");
 const authorizeRoles = require("../middleware/authMiddleware").authorizeRoles;
 const authenticate = require("../middleware/authMiddleware").authenticate;
@@ -14,6 +15,7 @@ const router = express.Router();
 router.post("/customer-register", customerRegister);
 router.post("/merchant-register", merchantRegister);
 router.post("/verify-email", verifyEmailCode);
+router.post("/resend-code", resendVerificationCode);
 router.post("/login", login);
 router.get("/getuser/:id", getUserById);
 //admin only
