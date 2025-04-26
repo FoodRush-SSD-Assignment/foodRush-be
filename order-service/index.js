5f7ad7f6740021ea699332812a45f4fe68c1ace4
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2025-04-26 18:36:58 +0530

    update cart controller

--- order-service/index.js
+++ order-service/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const connectDB = require("./config/db");
 require("dotenv").config();
+const cors = require("cors");
 
 const cartRoutes = require("./routes/cartRoutes.js");
 const orderRoutes = require("./routes/orderRoutes.js");
 
 const app = express();
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
