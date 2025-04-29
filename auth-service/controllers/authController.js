7a9fc5aff5bf50a1c1bc062c23a2aa577af98e74
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2025-04-29 21:17:10 +0530

    include firstname and lastname in JWT token payload

--- auth-service/controllers/authController.js
+++ auth-service/controllers/authController.js
@@ -214,7 +214,7 @@ exports.login = async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials" });
 
     const token = jwt.sign(
-      { userId: user._id, role: user.role },
+      { userId: user._id, role: user.role, firstname: user.firstname, lastname: user.lastname },
       process.env.JWT_SECRET,
       { expiresIn: "1d" }
     );
